7efa963e56620b9e52f4b821ce7c895ed95ec120
FatemeSfariSarvandi <safari.sv.fa@gmail.com>
2021-12-10 16:38:57 +0330

    add new file

---
+++ Teacher_requests/Script/script.js
@@ -0,0 +1,48 @@
+let check1 = document.getElementById("check1");
+let times1 = document.getElementById("times1");
+let check2 = document.getElementById("check2");
+let times2 = document.getElementById("times2");
+let check3 = document.getElementById("check3");
+let times3 = document.getElementById("times3");
+let check4 = document.getElementById("check4");
+let times4 = document.getElementById("times4");
+let check5 = document.getElementById("check5");
+let times5 = document.getElementById("times5");
+let check6 = document.getElementById("check6");
+let times6 = document.getElementById("times6");
+check1.onclick = ()=>{
+    times1.style.display = 'none';
+}
+times1.onclick = ()=>{
+    check1.style.display = 'none';
+}
+check2.onclick = ()=>{
+    times2.style.display = 'none';
+}
+times2.onclick = ()=>{
+    check2.style.display = 'none';
+}
+check3.onclick = ()=>{
+    times3.style.display = 'none';
+}
+times3.onclick = ()=>{
+    check3.style.display = 'none';
+}
+check4.onclick = ()=>{
+    times4.style.display = 'none';
+}
+times4.onclick = ()=>{
+    check4.style.display = 'none';
+}
+check5.onclick = ()=>{
+    times5.style.display = 'none';
+}
+times5.onclick = ()=>{
+    check5.style.display = 'none';
+}
+check6.onclick = ()=>{
+    times6.style.display = 'none';
+}
+times6.onclick = ()=>{
+    check6.style.display = 'none';
+}
